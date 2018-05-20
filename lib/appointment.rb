49c1e96e96ca6ca53dca0f0ed3173c2306038fe9
Kazumi Karbowski <kazkazinus@gmail.com>
2018-05-20 19:39:44 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,9 +4,9 @@ class Appointment
   @@all = []
 
   def initialize(patient, doctor, date)
-    @date = date
-    @doctor = doctor
     @patient = patient
+    @doctor = doctor
+    @date = date
     @@all << self
   end
 
